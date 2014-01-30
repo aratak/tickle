18dbd0aaa696b96fde43da53ca6b371f3331c9f3
Michal Muskala <michal.muskala@thesoftwarehouse.pl>
2014-01-30 16:40:28 +0100

    Improve bump methods to avoid argument aou of range error

--- lib/tickle.rb
+++ lib/tickle.rb
@@ -45,53 +45,35 @@ class Date #:nodoc:
     d - 1
   end
 
-  def bump(attr, amount=nil)
+  def bump(type, amount=nil)
     amount ||= 1
-    case attr
-    when :day then
-      Date.civil(self.year, self.month, self.day + amount)
+    case type
+    when :day, :week, :month, :year then
+      self + amount.send(type)
     when :wday then
       amount = Date::ABBR_DAYNAMES.index(amount) if amount.is_a?(String)
       raise Exception, "specified day of week invalid.  Use #{Date::ABBR_DAYNAMES}" unless amount
       diff = (amount > self.wday) ? (amount - self.wday) : (7 - (self.wday - amount))
-      Date.civil(self.year, self.month, self.day + diff)
-    when :week then
-      Date.civil(self.year, self.month, self.day + (7*amount))
-    when :month then
-      Date.civil(self.year, self.month+amount, self.day)
-    when :year then
-      Date.civil(self.year + amount, self.month, self.day)
+      self + diff.days
     else
-            raise Exception, "type \"#{attr}\" not supported."
+      raise Exception, "type \"#{type}\" not supported."
     end
   end
 end
 
 class Time #:nodoc:
-  def bump(attr, amount=nil)
+  def bump(type, amount=nil)
     amount ||= 1
-    case attr
-    when :sec then
-      Time.local(self.year, self.month, self.day, self.hour, self.min, self.sec + amount)
-    when :min then
-      Time.local(self.year, self.month, self.day, self.hour, self.min + amount, self.sec)
-    when :hour then
-      Time.local(self.year, self.month, self.day, self.hour + amount, self.min, self.sec)
-    when :day then
-      Time.local(self.year, self.month, self.day + amount, self.hour, self.min, self.sec)
+    case type
+    when :sec, :min, :hour, :day, :week, :month, :year then
+      self + amount.send(type)
     when :wday then
       amount = Time::RFC2822_DAY_NAME.index(amount) if amount.is_a?(String)
       raise Exception, "specified day of week invalid.  Use #{Time::RFC2822_DAY_NAME}" unless amount
       diff = (amount > self.wday) ? (amount - self.wday) : (7 - (self.wday - amount))
-      Time.local(self.year, self.month, self.day + diff, self.hour, self.min, self.sec)
-    when :week then
-      Time.local(self.year, self.month, self.day + (amount * 7), self.hour, self.min, self.sec)
-    when :month then
-      Time.local(self.year, self.month + amount, self.day, self.hour, self.min, self.sec)
-    when :year then
-      Time.local(self.year + amount, self.month, self.day, self.hour, self.min, self.sec)
+      self + diff.days
     else
-      raise Exception, "type \"#{attr}\" not supported."
+      raise Exception, "type \"#{type}\" not supported."
     end
   end
 end
